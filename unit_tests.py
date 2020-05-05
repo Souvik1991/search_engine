a78443fd18d9cff16ffcb19dc5a255c878af12a3
Souvik Maity <souvik.maity200@gmail.com>
2020-05-05 13:32:55 +0530

    updated unit test

--- unit_tests.py
+++ unit_tests.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
 	assert get_unique_bids([0, 0, 23, 3, 23, 1]) == [0, 1, 3, 23]
 	assert create_fuzzy_words('prob', ['problems', 'problem', 'prob']) == ['prob', 'problems', 'problem']
 	assert create_fuzzy_words('problems', ['problems', 'problem', 'prob']) == ['problems', 'problem']
-	assert str(calculate_score({'problems': ['problems', 'problem']}, ['problems'], {'word_count': 56, 'mapping': {'problems': {'position': [16, 28], 'appearance': 2}}})) == '2.34'
-	assert search_engine("problems", 1) == [{u'title': u'Slipstream Time Hacking', u'summary': u'The Book in Three Sentences:\xa0Finding something important and meaningful in your life is the most productive use of your time and energy. This is true because every life has problems associated with it and finding meaning in your life will help you sustain the effort needed to overcome the particular problems you face. Thus, we can say that the key to living a good life is not giving a fuck about more things, but rather, giving a fuck only about the things that align with your personal values.', 'score': 2.485, 'id': 48}]
+	assert str(calculate_score({'problems': ['problems', 'problem']}, ['problems'], {'word_count': 56, 'mapping': {'problems': {'position': [16, 28], 'appearance': 2}}})) == '17.34'
+	assert search_engine("problems", 1) == [{u'title': u'Slipstream Time Hacking', u'summary': u'The Book in Three Sentences:\xa0Finding something important and meaningful in your life is the most productive use of your time and energy. This is true because every life has problems associated with it and finding meaning in your life will help you sustain the effort needed to overcome the particular problems you face. Thus, we can say that the key to living a good life is not giving a fuck about more things, but rather, giving a fuck only about the things that align with your personal values.', 'score': 17.34, 'id': 48}]
 
-	
+
 # assert correction('speling') == 'spelling' 
