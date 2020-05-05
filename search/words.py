60ad1a3ebc6fe53d1d3f21136465376621c01b1f
Souvik Maity <souvik.maity200@gmail.com>
2020-05-05 14:58:23 +0530

    after chucking the common word filter

--- search/words.py
+++ search/words.py
@@ -1,3 +1,4 @@
 conjunctions = ['and', 'or', 'but', 'nor', 'yet', 'so', 'because']
 common_words = ['of', 'was', 'were', 'is', 'are', 'the', 'has', 'have', 'be', 'to', 'a', 'an', 'in', 'that', 'it', 'for', 'on', 'as', 'with', 'by', 'if']
-all_words = common_words + conjunctions
+# all_words = common_words + conjunctions
+all_words = []
